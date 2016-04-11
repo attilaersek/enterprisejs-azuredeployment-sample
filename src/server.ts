76a748dfdcf2256d2626c259106ff7c21c93395e
Attila Ersek <ersek.attila@hotmail.com>
2016-04-11 21:59:08 +0200

    applicationinsights telemetry basics

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import {Request, Response} from 'express';
 import express = require('express');
+import appInsights = require('applicationinsights');
+
+var start = +new Date;
 
 var port: number = process.env.PORT || 1337;
+var instrumentation: boolean = false;
+
+if(!!process.env.APPINSIGHTS_KEY) {
+  appInsights.setup(process.env.APPINSIGHTS_KEY).start();
+  instrumentation = true;
+}
+
 var app = express();
 
 var renderIndex = (req: Request, res: Response) => {
@@ -12,5 +22,10 @@ app.get('/*', renderIndex);
 
 var server = app.listen(port, function() {
     var port = server.address().port;
+    var end = +new Date;
+    var duration = end - start;
     console.log('This express app is listening on port:' + port);
+    if(instrumentation === true) {
+        appInsights.client.trackMetric('StartupTime', duration);
+    }
 });
